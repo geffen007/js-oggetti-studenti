0bdcc7c918dbcf2f6115db0275915edcbbd19246
Guido <guido.amodio@gmail.com>
2020-08-26 17:00:52 +0200

    cartella Bonus

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -44,16 +44,30 @@ $(document).ready(function() {
         }
     ];
 
-    for (var i = 0; i < classe.length; i++){
-        var student = $('.template .studente').clone();
-        student.find('.nome h2').append('Nome: ' + classe[i].nome);
-        student.find('.cognome h4').append('Cognome: ' + classe[i].cognome);
-        student.find('.eta h4').append('Età: ' + classe[i].eta);
-        console.log(student);
-    // console.log(classe[i].nome);
-    // console.log(classe[i].cognome);
-        $('.classe').append(student);
+    // for (var i = 0; i < classe.length; i++){
+    //     var student = $('.template .studente').clone();
+    //     student.find('.nome h2').append('Nome: ' + classe[i].nome);
+    //     student.find('.cognome h4').append('Cognome: ' + classe[i].cognome);
+    //     student.find('.eta h4').append('Età: ' + classe[i].eta);
+    //     console.log(student);
+    // // console.log(classe[i].nome);
+    // // console.log(classe[i].cognome);
+    //     $('.classe').append(student);
+    // }
+
+    for (var i = 0; i < classe.length; i++) {
+        var source = $('#entry-template').html();
+        var template = Handlebars.compile(source);
+
+
+        var html = template(classe[i]);
+
+        $('.classe').append(html);
+
     }
+
+
+
 
     //Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
@@ -70,14 +84,19 @@ $(document).ready(function() {
         newStudent.eta = prompt('inserisci l\'eta');
         classe.push(newStudent);
 
-            var student = $('.template .studente').clone();
-            student.find('.nome h2').append('Nome: ' + classe[(classe.length-1)].nome);
-            student.find('.cognome h4').append('Cognome: ' + classe[(classe.length-1)].cognome);
-            student.find('.eta h4').append('Età: ' + classe[(classe.length-1)].eta);
-            console.log(student);
-        // console.log(classe[i].nome);
-        // console.log(classe[i].cognome);
-            $('.classe').append(student);
+            // var student = $('.template .studente').clone();
+            // student.find('.nome h2').append('Nome: ' + classe[(classe.length-1)].nome);
+            // student.find('.cognome h4').append('Cognome: ' + classe[(classe.length-1)].cognome);
+            // student.find('.eta h4').append('Età: ' + classe[(classe.length-1)].eta);
+            // $('.classe').append(student);
+            var source = $('#entry-template').html();
+            var template = Handlebars.compile(source);
+
+
+            var html = template(classe[classe.length-1]);
+
+            $('.classe').append(html);
+
         }
 
 
